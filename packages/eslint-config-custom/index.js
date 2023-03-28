dae665bb2fc66db65dba08c9721f7cdbbcbcbb0e
Daniel Perez Alvarez <daniel@unindented.org>
2023-03-27 22:16:32 -0700

    feat: extend recommended ESLint presets

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,14 +1,20 @@
 module.exports = {
   extends: [
-    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:storybook/recommended",
     "plugin:storybook/csf-strict",
+    "next/core-web-vitals",
     "turbo",
     "prettier",
   ],
   plugins: ["simple-import-sort", "testing-library"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_", caughtErrorsIgnorePattern: "^_" },
+    ],
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
   },
@@ -18,6 +24,12 @@ module.exports = {
     },
   },
   overrides: [
+    {
+      files: ["**/*.config.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
     {
       files: ["**/*.e2e.{js,jsx,ts,tsx}"],
       extends: ["plugin:playwright/playwright-test"],
